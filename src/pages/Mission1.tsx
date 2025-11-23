5a343744ad9ca27c6b963db4b42b14f4e76989ee
Yongjooon <95464174+Yongjooon@users.noreply.github.com>
2025-11-23 20:55:42 +0900

    [feat] url 수정

--- src/pages/Mission1.tsx
+++ src/pages/Mission1.tsx
@@ -9,7 +9,9 @@ const Mission1: React.FC = () => {
   const [answer, setAnswer] = useState('');
   const [loading, setLoading] = useState(false);
 
-  const API_BASE_URL = 'http://3.36.53.174:81';
+      const API_BASE_URL = import.meta.env.PROD
+  ? '/api'
+  : (import.meta.env.VITE_API_BASE_URL ?? 'http://3.36.53.174:81')
 
   useEffect(() => {
     const storedIndex = getUserIndex();
